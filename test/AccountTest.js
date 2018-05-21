df904b37a799e3727301c1f6ff06fa52d60973cc
Irbe Krumina <irbekrm@gmail.com>
2018-05-21 13:10:12 +0100

    testing deposit

--- test/AccountTest.js
+++ test/AccountTest.js
@@ -1,7 +1,17 @@
 const chai = require('chai'),
+      sinon = require('sinon'),
       expect = chai.expect;
 
-var Account;
+var Account,
+    now,
+    clock;
+
+before(done => {
+  now = Date.now();
+  clock = sinon.useFakeTimers(now);
+  done();
+});
+
 
 beforeEach(done => {
   Account = require('../Account');
@@ -11,6 +21,14 @@ beforeEach(done => {
 describe('creating new account', _ => {
   it('is initialized with empty list of transactions', done => {
     expect(Account.statement()).to.equal('No transactions available');
+    done();
+  });
+});
+
+describe('deposit money into account', _ => {
+  it('adds the transaction to the list of transactions', done => {
+   Account.deposit(300.00);
+    expect(Account.statement()).to.equal(`date || credit || debit || balance\n${now} || 300.00 || || 300.00\n`);
     done();
   });
 });
