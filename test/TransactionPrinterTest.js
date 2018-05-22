b4cd6922bfbcb59e7c09da7d77126fe81ba99405
Irbe Krumina <irbekrm@gmail.com>
2018-05-22 10:13:59 +0100

    test that transactionPrinter formats time correctly

--- test/TransactionPrinterTest.js
+++ test/TransactionPrinterTest.js
@@ -1,19 +1,21 @@
 const chai = require('chai'),
       expect = chai.expect,
+      moment = require('moment'),
       tPrinter = require('../src/TransactionPrinter');
 
-var now,
-    clock;
+var formattedDate,
+    now
 
 before(done => {
   now = Date.now();
+  formattedDate = moment(now).format("DD/MM/YY"); 
   done();
 });
 
 describe('transaction printer', _ => {
   it('pretty prints a transaction', done => {
     const transaction = { balance: 200.00, amount: 200.00, date: now, transactionType: 'withdraw' };
-    const expected = `${now} ||  || 200.00 || 200.00\n`;
+    const expected = `${formattedDate} ||  || 200.00 || 200.00\n`;
     expect(tPrinter.prettyPrint(transaction)).to.equal(expected);
     done();
   });
