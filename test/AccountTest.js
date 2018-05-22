789a5d48b0c89843204377a555ab7691e4c1f462
Irbe Krumina <irbekrm@gmail.com>
2018-05-22 15:55:42 +0100

    transaction printer stubbed with sinon

--- test/AccountTest.js
+++ test/AccountTest.js
@@ -4,23 +4,31 @@ const chai = require('chai'),
   expect = chai.expect,
   stdout = require('test-console').stdout,
   moment = require('moment'),
+  sinon = require('sinon'),
   header = 'date || credit || debit || balance\n',
+  TransactionPrinter = require('../models/TransactionPrinter'),
   Account = require('../models/Account');
 
 var now,
   account,
-  formattedDate;
+  formattedDate,
+  prettyPrinter;
 
-before(done => {
+before(function(done){
+
+    //.onCall(2).returns(`${formattedDate} || 300.00 ||  || 300.00\n`);
   now = Date.now();
   formattedDate = moment(now).format('DD/MM/YYYY');
   done();
 });
 
-beforeEach(done => {
+beforeEach(function(done){
+
+    //.onCall(1).returns(`${formattedDate} ||  || 140.00 || 160.00\n`)
   account = new Account.Account();
   done();
 });
+
 
 describe('creating new account', _ => {
   it('is initialized with empty list of transactions', done => {
@@ -41,8 +49,12 @@ describe('checking for negative amounts', _ => {
   });
 });
 
-describe('deposit money into account', _ => {
-  it('adds the transaction to the list of transactions', done => {
+describe('deposit money into account', function() {
+  it('adds the transaction to the list of transactions',function(done){
+    sinon.stub(TransactionPrinter, 'prettyPrint')
+    .onCall(0).returns(`${formattedDate} || 300.00 ||  || 300.00\n`)
+    .onCall(1).returns(`${formattedDate} ||  || 140.00 || 160.00\n`)
+    .onCall(2).returns(`${formattedDate} || 300.00 ||  || 300.00\n`);
     account.deposit(300.00);
     const statement = stdout.inspectSync(_ => account.printStatement()),
       expected = [`${header}${formattedDate} || 300.00 ||  || 300.00\n\n`];
@@ -51,8 +63,8 @@ describe('deposit money into account', _ => {
   });
 });
 
-describe('withdraw money from account', _ => {
-  it('adds the transaction to the list of transactions', done => {
+describe('withdraw money from account', function(){
+  it('adds the transaction to the list of transactions',function(done){
     account.deposit(300.00).withdraw(140.00);
     const statement = stdout.inspectSync(_ => account.printStatement()),
       expected = [`${header}${formattedDate} ||  || 140.00 || 160.00` +
