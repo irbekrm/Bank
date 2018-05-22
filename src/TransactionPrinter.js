8146cf56cbe7d8b3936c94b7ff1277fb763ee9e3
Irbe Krumina <irbekrm@gmail.com>
2018-05-22 10:23:59 +0100

    date formatted correctly for printing

--- src/TransactionPrinter.js
+++ src/TransactionPrinter.js
@@ -1,12 +1,17 @@
+const moment = require('moment');
+
 exports.prettyPrint = data => {
   const credit = isCredit(data);
   const debit = isDebit(data);
-  const balance = format(data.balance);
-  return `${data.date} || ${credit} || ${debit} || ${balance}\n`;
+  const date = formatDate(data.date);
+  const balance = formatFloat(data.balance);
+  return `${date} || ${credit} || ${debit} || ${balance}\n`;
 }
 
-const format = number => number.toFixed(2);
+const formatFloat = number => number.toFixed(2);
 
-const isCredit = data => data.transactionType == 'deposit' ? format(data.amount) : '';
+const formatDate = date => moment(date).format("DD/MM/YY");
 
-const isDebit = data => data.transactionType == 'withdraw' ? format(data.amount) : '';
+const isCredit = data => data.transactionType == 'deposit' ? formatFloat(data.amount) : '';
+
+const isDebit = data => data.transactionType == 'withdraw' ? formatFloat(data.amount) : '';
